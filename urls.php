805ff244511d57bcc5fac923da90c6573f3d3a92
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-01-21 04:57:52 +0100

    Fxes on urls

--- urls.php
+++ urls.php
@@ -3,7 +3,8 @@
 PhangoVar::$urls['wserver']['login']=array('pattern' => '/^wserver\/login\/(\w+)$/', 'url' => '/wserver/login', 'module' => 'wserver2', 'controller' => 'admin', 'action' => 'login', 'parameters' => array('$1' => 'string'));
 
 //Load urls for every module.
-//Structure is controlers/servers/so/so_version/type_server_codename/controllers_folder_name/
+//Structure is controlers/servers/so/so_version/type_server_codename/controllers_folder_name/controller file
+//Use cache?
 
 
 ?>
